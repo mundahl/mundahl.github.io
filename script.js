65877724f7c6a39b03f4cf83bd2c17c02bcd3e20
JohnM <john.mundahl+git@gmail.com>
2021-08-03 18:20:20 -0500

    combinations of numbers
    title

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var a = prompt("First name pls");
-if (a === "Haha") {
-  alert("Hello there, " + a);
+var a = ["1", "2"];
+var b = ["5", "6"];
+var emptyArr = [];
+var c = 0;
+for (var aa = 0; aa < a.length; aa++) {
+  for (var bb = 0; bb < b.length; bb++) {
+    emptyArr[c] = a[aa] + b[bb];
+    c++;
+  }
+}
+for (var cc = 0; cc < emptyArr.length; cc++) {
+  alert(emptyArr[cc])
 }
